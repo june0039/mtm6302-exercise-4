45198a97631ff97de31700845324a17f35700fe3
june0039 <74568248+june0039@users.noreply.github.com>
2022-04-04 14:54:30 -0400

    script

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 /* Counter Assignment Script JS page 
 By: Julie Juneau Student # 040930994 */
+
+const $form = document.getElementById('form')
+const day = document.getElementById('day')
+const now = DateTime.local();
+
+function setDays() {
+    
+    const date = DateTime.fromObject({year: $year.value, month:value })
+
+}
+
+for (let i = 1; i <= date.daysInMonth; i++){
+    days.push('<option>${i}</option>')
+}
+{
+
+    $day.innerHTML = days.join('')
+    
+}
+
+$form.addEventListener('submit', function (e) {
+
+    e.preventDefault()
+
+    const date = DateTime.fromObject({ year: $year.value })
+    /* Check if date is in the future and provide amount*/
+    if (date > now) {
+
+        const diff = date.diff(now, ['days', 'hours', 'minutes', 'seconds']).toObject()
+        $response.textContent = `${diff.days} days
+        ${diff.hours} hours ${diff.minutes} minutes`
+    }
+    
+}
+    
+})
