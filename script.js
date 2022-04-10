610f3a4f0417504c5b728fbe2fbede873954cb4a
june0039 <74568248+june0039@users.noreply.github.com>
2022-04-10 15:14:50 -0400

    end

--- script.js
+++ script.js
@@ -4,55 +4,60 @@ By: Julie Juneau Student # 040930994 */
 const $form = document.getElementById('form')
 const $countDown = document.getElementById('countDown')
 const $clear = document.getElementById('clear')
-const now = new Date();
+const now = new Date()
 const $userTitle = document.getElementById('userTitle')
-
+let id = null
 let start = now
 
-if (localStorage.getItem('data') != undefined) {
+if (localStorage.getItem('newDate') && localStorage.getItem('title')) {
 
-  let data = JSON.parse(localStorage.getItem('data'))
 
-  $form.elements.title.value = data.title
-  $form.elements.date.value = data.date
+  $form.elements.title.value = localStorage.getItem('title')
+  $form.elements.date.value = localStorage.getItem('newDate')
+
+  $form.classList.add('hide')
+  $userTitle.classList.remove('hide')
+
+  id = setInterval(display, 1000)
+display()
+  
   
 }
 
-//function setDays() {
-    
-  
-
 /* Function when count down button is clicked */
-$countDown.addEventListener('click', function () {
+$countDown.addEventListener('click', function (e) {
 
-  
+e.preventDefault()
 
-  e.preventDefault()
 
-  let newDate = dateDiff(now, new Date($form.elements.date.value))
   localStorage.setItem('newDate', $form.elements.date.value)
   localStorage.setItem('title', $form.elements.title.value)
+
   $form.classList.add('hide')
+  $userTitle.classList.remove('hide')
 
-  setInterval(function () {
+  id = setInterval(display, 1000)
+  display()
     
-    //Display new form of countdown date and time with the title entered by the user
-
-    newDate  = dateDiff(new Date(), new Date($form.elements.date.value))
-
-    $userTitle.innerHTML = `<h1>${$form.elements.title.value}</h1>
-    <p>${newDate.days} : ${newDate.hours} : ${newDate.minutes} : ${newDate.seconds} </p>
-    <div>
-    <span>days</span> <span>hours</span> <span>minutes</span> <span> seconds</span>
-    </div>
-    <button class="clearCount" id="clear"> Clear Countdown</button>`
-  }, 1000)
-
 })
+
+function display() {
+
+//Display new form of countdown date and time with the title entered by the user
+
+  newDate = dateDiff(new Date(), new Date($form.elements.date.value))
+  $userTitle.innerHTML = `<h1>${$form.elements.title.value}</h1>
+ <p>${newDate.days} : ${newDate.hours} : ${newDate.minutes} : ${newDate.seconds} </p>
+ <div>
+ <span>days</span> <span>hours</span> <span>minutes</span> <span> seconds</span>
+ </div>
+ <button class="clearCount" id="clear"> Clear Countdown</button>`
+}
 
 //Function to calculate the difference between todays date and the users input date
 
-    function dateDiff (start, end) {
+function dateDiff(start, end) {
+      
   const diff = end - start > 0 ? end - start : 0
   const format = (num) => num < 10 ? `0${num}` : num
   
@@ -64,22 +69,35 @@ $countDown.addEventListener('click', function () {
   }
 }
     
+
 //Clear button function to clear counter and data from local storage and display entry form again
 
-$clear.addEventListener('click', function (e) {
+$userTitle.addEventListener('click', function (e) {
 
-  let $clear = 1;
+  if (e.target.classList.contains("clearCount")){
+    
+    clearInterval(id)
   
-  //$form.elements.title.value = ''
-
- // $form.classList.remove('hide')
- // $userTitle.classList.add('hide')
+    //hide title and counter
+    $userTitle.classList.add('hide')
   
-  localStorage.clear();
+    //Display starting form
+    $form.classList.remove('hide')
+
+    //Reset form to empty
+    $form.reset()
+
+    localStorage.clear()
+
+  }
+
+  })
 
 
 
-})
+
+
+
 
 
 
